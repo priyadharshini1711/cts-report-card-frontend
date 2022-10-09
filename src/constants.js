85965866944aed58f7ad7682b742a7e497f1ffe9
priyadharshini1711 <priyadharshini170898@gmail.com>
2022-10-09 20:53:27 +0530

    added href

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 import { HomeIcon, UsersIcon, AcademicCapIcon, CalendarIcon, ViewBoardsIcon, TableIcon, ViewListIcon } from "@heroicons/react/outline";
 
 export const sidebar_menu = [
-    { name: 'Dashboard', href: 'https://cts-report-card.netlify.app/dashboard', icon: HomeIcon, current: true },
-    { name: 'Students', href: 'https://cts-report-card.netlify.app/student', icon: UsersIcon, current: false },
-    { name: 'Colleges', href: 'https://cts-report-card.netlify.app/college', icon: AcademicCapIcon, current: false },    
-    { name: 'Months', href: 'https://cts-report-card.netlify.app/month', icon: ViewBoardsIcon, current: false },
-    { name: 'Events', href: 'https://cts-report-card.netlify.app/event', icon: CalendarIcon, current: false },
-    { name: 'College Events', href: 'https://cts-report-card.netlify.app/college-events', icon: ViewListIcon, current: false },
-    { name: 'Rank Table', href: 'https://cts-report-card.netlify.app/rank_table', icon: TableIcon, current: false },
+    { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true },
+    { name: 'Students', href: '/student', icon: UsersIcon, current: false },
+    { name: 'Colleges', href: '/college', icon: AcademicCapIcon, current: false },    
+    { name: 'Months', href: '/month', icon: ViewBoardsIcon, current: false },
+    { name: 'Events', href: '/event', icon: CalendarIcon, current: false },
+    { name: 'College Events', href: '/college-events', icon: ViewListIcon, current: false },
+    { name: 'Rank Table', href: '/rank_table', icon: TableIcon, current: false },
 ]
 
 export const MODE = {
