1ae131657ce3949c90d10e94f14391ab595c013d
priyadharshini1711 <priyadharshini170898@gmail.com>
2022-10-11 23:28:34 +0530

    without server

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 import { HomeIcon, UsersIcon, AcademicCapIcon, CalendarIcon, ViewBoardsIcon, TableIcon, ViewListIcon } from "@heroicons/react/outline";
 
 export const sidebar_menu = [
-    { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true },
+    { name: 'Dashboard', href: '/', icon: HomeIcon, current: true },
     { name: 'Students', href: '/student', icon: UsersIcon, current: false },
     { name: 'Colleges', href: '/college', icon: AcademicCapIcon, current: false },    
     { name: 'Months', href: '/month', icon: ViewBoardsIcon, current: false },
